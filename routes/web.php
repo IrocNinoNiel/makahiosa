089978a1730ba7504326420ddcc3ddc97ba7cdeb
IrocNinoNiel <iroc.niel123@gmail.com>
2022-07-05 21:36:11 +0800

    Fixing timeline routes

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::group(['middleware' => 'auth'], function() {
 
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/timeline', [App\Http\Controllers\TimelineController::class, 'index'])->name('timeline');
+    Route::get('/events', [App\Http\Controllers\TimelineController::class, 'getAllEvent'])->name('getAllEvent');
     Route::get('/organization', [App\Http\Controllers\HomeController::class, 'showOrganization'])->name('organization');
 
     Route::get('/orglist', [App\Http\Controllers\HomeController::class, 'showOrgList'])->name('orglist');
